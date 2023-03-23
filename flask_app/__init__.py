cb6d8209d293da572b66b69927b31cca616ec662
Jsingh651 <108104100+Jsingh651@users.noreply.github.com>
2023-03-22 20:24:05 -0700

    Update __init__.py

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask_mail import Mail
 import os
 app = Flask(__name__)
-app.secret_key = 'helefvefvr4332432lo'
+app.secret_key = '1'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'jangsing02@gmail.com'
-app.config['MAIL_PASSWORD'] = 'zkgfklffuiukdcio'
+app.config['MAIL_USERNAME'] = '111.com'
+app.config['MAIL_PASSWORD'] = '1111'
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USE_TLS'] = False
 mail = Mail(app)

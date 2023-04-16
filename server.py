9a8afb8280344a9c6860d871d5aba0508c62869a
Jsingh651 <jangsing02@gmail.com>
2023-04-16 14:14:16 -0700

    fd

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
 from flask_app import app
 from flask_app.controllers import userController, tripController
 from flask import Flask
-from flaskext.mysql import MySQL
-
-app = Flask(__name__)
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_DB'] = 'solo_project'
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'rootroot'
+from flask_mysqldb import MySQL
+import os
+from dotenv import load_dotenv
+load_dotenv()
+app.config['MYSQL_DATABASE_HOST'] = os.environ.get('MYSQL_DATABASE_HOST', 'localhost')
+app.config['MYSQL_DATABASE_PORT'] = int(os.environ.get('MYSQL_DATABASE_PORT', 3306))
+app.config['MYSQL_DATABASE_DB'] = os.environ.get('MYSQL_DATABASE_DB', 'solo_project')
+app.config['MYSQL_DATABASE_USER'] = os.environ.get('MYSQL_DATABASE_USER', 'root')
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ.get('MYSQL_DATABASE_PASSWORD', 'rootroot')
 
 mysql = MySQL()
 mysql.init_app(app)
